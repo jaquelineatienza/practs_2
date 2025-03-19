dcace2b3f323f16ab13849138dfb83d259fea1a4
jaquelineatienza <jaquibatienza.com>
2025-03-19 17:15:44 -0300

    se termino el trabajo

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 date =  [];
 
-nombre = input("Ingresa tu nombre: ")
-nombre = int("Ingresa tu edad: ")
-nombre = int("Ingresa tu nota: ")
+n = int(input('ingrese la cantidad de personas a cargar'))
 
-print(nombre)
+i = 0
+while i < n:
+ 
+    nombre = input("Ingresa tu nombre: ")
+    edad = int(input("Ingresa tu edad: "))
+    nota = int(input("Ingresa tu nota: ")) 
+    # date.append(nombre,edad,nota)
+    newDate = [nombre,edad,nota]
+    date.append(newDate)
+    i += 1
+ 
+
+
+# print(nombre,edad,nota)
+datos_ordenados = sorted(date, key=lambda x: x[2], reverse=True)
+print(datos_ordenados)
